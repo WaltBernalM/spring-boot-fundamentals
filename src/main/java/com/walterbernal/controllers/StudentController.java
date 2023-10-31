dee58e57107314d6d73fbfb1bb42ca3efb49e19a
Walter Bernal <wltrbm@gmail.com>
2023-10-31 16:50:37 -0700

    PostMapping to add student

--- src/main/java/com/walterbernal/controllers/StudentController.java
+++ src/main/java/com/walterbernal/controllers/StudentController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.walterbernal.beans.Student;
@@ -22,15 +25,31 @@ public class StudentController {
     students.add(new Student("Gautam", "Bhalla"));
   }
 
+  // http://localhost:8080/students
   @GetMapping("/students")
   public List<Student> getStudents() {
     return students;
   }
 
+  // http://localhost:8080/student/Walter/Bernal
   @GetMapping("/student/{firstName}/{lastName}")
   public Student studentPathVariable(@PathVariable("firstName") String firstName,
       @PathVariable("lastName") String lastName) {
-        return new Student(firstName, lastName);
+    return new Student(firstName, lastName);
+  }
+  
+  // http://localhost:8080/student/query?firstName=John&lastName=Smith
+  @GetMapping("/student/query")
+  public Student studentQueryParam(@RequestParam(name = "firstName") String firstName,
+      @RequestParam(name = "lastName") String lastName) {
+    return new Student(firstName, lastName);
+  }
+  
+  // http://localhost:8080/student
+  @PostMapping("/student")
+  public List<Student> addStudent(@RequestBody Student student) {
+    students.add(student);
+    return students;
   }
 
 }
